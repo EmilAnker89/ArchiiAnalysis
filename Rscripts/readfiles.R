041710c65ff71252fe432c435aaf27c8f1d46783
EmilAnker89 <emilajorgensen@hotmail.com>
2017-03-28 16:35:49 +0200

    indlæs features

--- Rscripts/readfiles.R
+++ Rscripts/readfiles.R
@@ -10,33 +10,14 @@ files <- files[!grepl(".py",files)]
 
 file <- files[[1]]
 file2 <- files[[2]]
+file3 <- files[[3]]
 train <- read.csv2(paste0("out/data_import/",file), header=F, stringsAsFactors = F)
 label <- read.table(paste0("out/data_import/",file2), header=F, stringsAsFactors = F)
-contract <- cbind(train,label)
-#rm(train, label, file, file2)
+features <- read.table(paste0("out/data_import/",file3), header=F, stringsAsFactors = F)[,1]
+
+#contract <- cbind(train,label)
 train <- lapply(train, as.numeric) %>% as.data.frame
-
-# Kmeans clustre analysis
-clus <- kmeans(train, centers=2)
-
-# Fig 01
-plotcluster(train, clus$cluster)
-clusplot(train, clus$cluster, color=TRUE, shade=TRUE,
-         labels=2, lines=0)
-
-
-#with(contract, pairs(train, col=c(1:5)[clus$cluster]))
-
-
-# apply PCA - scale. = TRUE is highly
-# advisable, but default is FALSE.
-combi$Item_Visibility <- ifelse(combi$Item_Visibility == 0, median(combi$Item_Visibility),                                   combi$Item_Visibility)
-
-# train2 <- lapply(train, function(x) {
-#   m <- median(x,na.rm=T)
-#   ifelse(x==0,m,x)
-# }) %>% as.data.frame
-
+names(train) <- features
 
 train.pca <- prcomp(train,
                  center = TRUE,
@@ -62,6 +43,14 @@ results <- apply(train.pca$x[,1:5], 2, function(x) {
   noncon <- median(x[label$color=="blue"], na.rm=T)
   c(con=con, noncon=noncon)
 })
+
+library(dplyr)
+#now make your lovely plot
+temp <- data.frame(component = train.pca$x[,1], type=label$V1)
+ggplot(temp, aes(component, fill = type)) +
+  geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity', bins=100) +
+  scale_x_continuous(limits = c(-.5, 1))
+#  scale_y_continuous(limits = c(0, 3))
 
 
 library(ggplot2)
