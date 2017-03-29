8a087ca25ae1b05017daaace6682448965394692
EmilAnker89 <emilajorgensen@hotmail.com>
2017-03-30 00:44:08 +0200

    leg med 3d plot

---
+++ R/read_data.R
@@ -0,0 +1,37 @@
+#' read_data
+#'
+#' @param train \code{character} path to training dataset (matrix) - must be .csv
+#' @param labels \code{character} path to training dataset (labels) - must be .txt
+#' @param features \code{character} path to training dataset (features) - must be .txt
+#'
+#' @return df \code{data.frame} returns a data.frame containing training data with labels and features.
+#' @export
+#'
+#' @examples
+#' \dontrun {
+#' dir("out/data_import") #show files in a directory
+#' train <- "out/data_import/contract_binarytrain_data.csv"
+#' labels <- "out/data_import/contract_binarytrain_labels.txt"
+#' features <- "out/data_import/contract_binarytrain_vocabulary.txt"
+#' df <- read_data(train, labels, features)
+#' }
+
+read_data <- function(train, labels, features) {
+  train <- read.csv2(train, header=F, stringsAsFactors = F)
+  labels <- read.table(labels, header=F, stringsAsFactors = F)[,1]
+  features <- read.table(features, header=F, stringsAsFactors = F)[,1]
+  df <- apply(train, 2, as.numeric) %>% as.data.frame
+  names(df) <- features
+  df[["contract_label"]] <- labels
+  df
+}
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+# dir("out/data_import")
+# train <- "out/data_import/contract_binarytrain_data.csv"
+# labels <- "out/data_import/contract_binarytrain_labels.txt"
+# features <- "out/data_import/contract_binarytrain_vocabulary.txt"
+
--- Rscripts/readfiles.R
+++ Rscripts/readfiles.R
@@ -1,41 +1,117 @@
-#indhent og gem træningsdata som .rds:
-library(cluster)
-library(HSAUR)
-library(fpc)
-library(magrittr)
 
+#dir("out/data_import") #show files in a directory
+train <- "out/data_import/contract_binarytrain_data.csv"
+labels <- "out/data_import/contract_binarytrain_labels.txt"
+features <- "out/data_import/contract_binarytrain_vocabulary.txt"
+df <- read_data(train, labels, features)
+non_labels <- setdiff(names(df),"contract_label")
+train <- df[,non_labels]
 
-files <- dir("out/data_import")
-files <- files[!grepl(".py",files)]
-
-file <- files[[1]]
-file2 <- files[[2]]
-file3 <- files[[3]]
-train <- read.csv2(paste0("out/data_import/",file), header=F, stringsAsFactors = F)
-label <- read.table(paste0("out/data_import/",file2), header=F, stringsAsFactors = F)
-features <- read.table(paste0("out/data_import/",file3), header=F, stringsAsFactors = F)[,1]
-
-#contract <- cbind(train,label)
-train <- lapply(train, as.numeric) %>% as.data.frame
-names(train) <- features
-
-train.pca <- prcomp(train,
+train.pca <- stats::prcomp(train,
                  center = TRUE,
-                 scale. = TRUE, retx=T)
-print(train.pca)
-plot(train.pca, type = "l", xlim=c(1,10))
-summary(train.pca)
-train.pca$rotation[1:5,1:10]
-sum(train.pca$sdev>5)
-label$color <- ifelse(label=="Contract", "red", "blue")
+                 scale. = FALSE, retx=T)
+#print(train.pca)
+plot(train.pca, type = "l", npcs=10)
+totvar <- sum(train.pca$sdev^2)
+var <- train.pca$sdev^2
+pct_var <- var/totvar
+#train.pca$rotation[1:5,1:10]
+#for top 10 PCs, find 20 features loading the highest:
+out <- list()
+for (i in 1:10) {
+  pc <- train.pca$rotation[,i]
+  pc_abs <- sort(abs(pc), decreasing=T)[1:20]
+  out[[paste0("pc",i)]] <- pc[match(names(pc_abs),names(pc))]
+}
+
+lvls <- unique(df$contract_label)
+colors <- grDevices::rainbow(length(lvls))
+col_vector <- colors[match(test, lvls)]
 #plot c1, c2 + labels
-xlim = c(quantile(train.pca$x[,1],.005),quantile(train.pca$x[,1],.995))
-ylim = c(quantile(train.pca$x[,2],.005),quantile(train.pca$x[,2],.995))
-plot(train.pca$x[,1],train.pca$x[,2], xlim=xlim,ylim=ylim, col=label$color)
-# plot c3, c4 + labels
-xlim = c(quantile(train.pca$x[,3],.005),quantile(train.pca$x[,3],.995))
-ylim = c(quantile(train.pca$x[,4],.005),quantile(train.pca$x[,4],.995))
-plot(train.pca$x[,3],train.pca$x[,4], xlim=xlim,ylim=ylim, col=label$color)
+#plot the two PCs with the highest median difference across:
+medians <- apply(train.pca$x[,1:10], 2, function(x) {
+  m <- lapply(lvls, function(y) {
+    subset <- match(df$contract_label,y)==1
+    median(x[subset], na.rm=T)
+  })
+  names(m) <- lvls
+  m[["max-diff"]] <-  max(unlist(m))-min(unlist(m))
+  m
+})
+pcs <- lapply(medians, "[[", "max-diff") %>% unlist %>% sort(.,decreasing=T)
+pcs <- match(names(pcs),names(medians))
+
+xlim = c(quantile(train.pca$x[,pcs[[1]]],.01),quantile(train.pca$x[,pcs[[1]]],.99))
+ylim = c(quantile(train.pca$x[,pcs[[2]]],.01),quantile(train.pca$x[,pcs[[2]]],.99))
+zlim = c(quantile(train.pca$x[,pcs[[3]]],.01),quantile(train.pca$x[,pcs[[3]]],.99))
+plot(train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[1]]],
+     train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[2]]],xlim=xlim,ylim=ylim, col=colors[[1]])
+abline(v=medians[pcs[[1]]][[1]][[1]], col=colors[[1]])
+abline(h=medians[pcs[[2]]][[1]][[1]], col=colors[[1]])
+
+points(train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[1]]],
+     train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[2]]], col=colors[[2]], add=T)
+abline(v=medians[pcs[[1]]][[1]][[2]], col=colors[[2]])
+abline(h=medians[pcs[[2]]][[1]][[2]], col=colors[[2]])
+
+
+plot(train.pca$x[,pcs[[1]]],train.pca$x[,pcs[[2]]], xlim=xlim,ylim=ylim, col=col_vector)
+abline(v=medians[pcs[[1]]][[1]][[1]], col=colors[[1]])
+abline(v=medians[pcs[[1]]][[1]][[2]], col=colors[[2]])
+abline(h=medians[pcs[[1]]][[1]][[2]], col=colors[[2]])
+
+
+
+library(scatterplot3d)
+s3d <- scatterplot3d(train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[1]]], xlim=xlim,
+                     train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[2]]], ylim=ylim,
+                     train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[3]]], zlim=zlim,
+                     color=colors[[1]])
+s3d$points3d(train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[1]]],
+             train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[2]]],
+             train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[3]]],
+             col=colors[[2]])
+
+library(rgl)
+
+s3d <- plot3d(train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[1]]], xlim=xlim,
+       train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[2]]], ylim=ylim,
+       train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[3]]], zlim=zlim,
+       col=colors[[1]], size=5,
+       xlab=paste0("Component 1: ",round(pct_var[1],4)*100," % Variance explained"),
+       ylab=paste0("Component 2: ",round(pct_var[2],4)*100," % Variance explained"),
+       zlab=paste0("Component 3: ",round(pct_var[3],4)*100," % Variance explained"))
+points3d(train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[1]]],
+             train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[2]]],
+             train.pca$x[match(df$contract_label,lvls[[2]]) %in% 1,pcs[[3]]],
+             col=colors[[2]], size=5)
+legend3d("topright", legend = paste('Type:', c(lvls[[1]], lvls[[2]])), pch = 16, col = colors, cex=1, inset=c(0.02))
+input <- select3d()
+
+xyz <- c(train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[1]]],
+train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[2]]],
+train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[3]]])
+
+
+
+
+
+
+
+
+library(Rcmdr)
+s3d <- scatter3d(train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[1]]], xlim=xlim,
+          train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[2]]], ylim=ylim,
+          train.pca$x[match(df$contract_label,lvls[[1]]) %in% 1,pcs[[3]]], zlim=zlim,
+          color=colors[[1]])
+
+
+
+
+
+
+#######
+
 
 test <- train.pca$x[,1:5]
 results <- apply(train.pca$x[,1:5], 2, function(x) {
