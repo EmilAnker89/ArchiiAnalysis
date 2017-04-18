8c9c9d96e6b8857f83b7cf219a8ad5ad944fb109
EmilAnker89 <emilajorgensen@hotmail.com>
2017-04-18 19:44:33 +0200

    lav to eksempler i prepplotlydata.R

--- Rscripts/prepplotlydata.R
+++ Rscripts/prepplotlydata.R
@@ -1,9 +1,9 @@
-#dir("out/data_import") #show files in a directory
-train <- "out/data_import/contract_cat_cooptrain_data.csv"
-labels <- "out/data_import/contract_cat_cooptrain_labels.txt"
-#features <- "out/data_import/contract_binarytrain_vocabulary.txt"
+dir("out/data_import") #show files in a directory
+train <- "out/data_import/contract_binarytrain_data.csv"
+labels <- "out/data_import/contract_binarytrain_labels.txt"
+features <- "out/data_import/contract_binarytrain_vocabulary.txt"
 
-df <- read_data(train=train, labels=labels)
+df <- read_data(train, labels, features)
 non_labels <- setdiff(names(df),"contract_label")
 train <- df[,non_labels]
 
@@ -11,10 +11,28 @@ train <- df[,non_labels]
 train.pca <- stats::prcomp(train,
                            center = TRUE,
                            scale. = FALSE, retx=T)
-saveRDS(train.pca$x %>% as.data.frame, "Rscripts/plotly_app/pca2.rds")
+
+#gem til brug i app:
+saveRDS(train.pca$x %>% as.data.frame, file = "Rscripts/plotly_app/pca.rds")
 labels <- read.table(labels, header=F, stringsAsFactors = F)[,1]
-saveRDS(labels, "Rscripts/plotly_app/labels2.rds")
+saveRDS(labels, file = "Rscripts/plotly_app/labels.rds")
 
+#prøv en anden (multiclass):
 
+train <- "out/data_import/contract_cattrain_data.csv"
+labels <- "out/data_import/contract_cattrain_labels.txt"
+features <- "out/data_import/contract_cattrain_vocabulary.txt"
 
+df <- read_data(train, labels, features)
+non_labels <- setdiff(names(df),"contract_label")
+train <- df[,non_labels]
 
+#kunne være en t-SNE i stedet for PCA (måske bedre til plots):
+train.pca <- stats::prcomp(train,
+                           center = TRUE,
+                           scale. = FALSE, retx=T)
+
+#gem til brug i app:
+saveRDS(train.pca$x %>% as.data.frame, file = "Rscripts/plotly_app/pca.rds")
+labels <- read.table(labels, header=F, stringsAsFactors = F)[,1]
+saveRDS(labels, file = "Rscripts/plotly_app/labels.rds")
